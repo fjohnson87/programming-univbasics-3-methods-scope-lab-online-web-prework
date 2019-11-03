afed592bd1136004308f5c51a1008db1d116a1b7
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-03 18:03:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,5 +5,14 @@ def mario
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  new_phrase = "It's Dangerous To Go Alone! Take This."
+  puts new_phrase
+end
+
+def any_phrase(phrase)
+  phrase = "Do a Barrel Roll!"
